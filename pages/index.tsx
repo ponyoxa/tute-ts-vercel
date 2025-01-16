b0f30031f6ddda4e7e8691bd2e2e2c1b12effb75
ponyoxa <92624736+ponyoxa@users.noreply.github.com>
2025-01-16 11:48:37 +0000

    SSRの手前まで

---
+++ pages/index.tsx
@@ -0,0 +1,36 @@
+import { NextPage } from "next";
+import { useEffect, useState } from "react";
+ 
+const IndexPage: NextPage = () => {
+    const [imageUrl, setImageUrl] = useState("");
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+      fetchImage().then((newImage) => {
+        setImageUrl(newImage.url); 
+        setLoading(false);
+      });
+    }, []);
+    const handleClick = async () => {
+        setLoading(true); // 読込中フラグを立てる
+        const newImage = await fetchImage();
+        setImageUrl(newImage.url); // 画像URLの状態を更新する
+        setLoading(false); // 読込中フラグを倒す
+      };
+      return (
+        <div>
+          <button onClick={handleClick}>他のにゃんこも見る</button>
+          <div>{loading || <img src={imageUrl} />}</div>
+        </div>
+      );
+  };
+  export default IndexPage;
+   
+  type Image = {
+    url: string;
+  };
+  const fetchImage = async (): Promise<Image> => {
+    const res = await fetch("https://api.thecatapi.com/v1/images/search");
+    const images = await res.json();
+    console.log(images);
+    return images[0];
+  };
